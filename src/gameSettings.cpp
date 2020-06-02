f74ec525f9660364b34a9d588b68cc902875f7d7
Luan de Barros <luanbarrossantos@gmail.com>
2020-06-02 13:45:38 -0300

    variable class changed

--- src/gameSettings.cpp
+++ src/gameSettings.cpp
@@ -9,17 +9,17 @@ void gameSettings::showSlender(int time_elaps) {
 		times += 0.5;
 		frequency += (times * 10);
 		
-		//cout << "passs here" << endl;
+		cout << "passs here" << endl;
 		//cout << "passs here" << endl;
 		//cout << "passs here" << endl;
 
 		shaderProgram.use();
 		camera.transform(&shaderProgram);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-		shaderProgram.setMat4("model", model);
+		glm::mat4 matriz_model = glm::mat4(1.0f);
+		matriz_model = glm::translate(matriz_model, glm::vec3(0.0f, -1.0f, 0.0f));
+		matriz_model = glm::scale(matriz_model, glm::vec3(1.0f, 1.0f, 1.0f));
+		shaderProgram.setMat4("model", matriz_model);
 		model.Draw(shaderProgram);
 	}
 }
