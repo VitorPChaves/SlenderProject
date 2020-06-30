872f5e2119a62b86589f0cebca20c41143f5063e
Vitor Patricio Chaves <vitor.patricio@audaces.com>
2020-06-30 13:33:21 -0300

    Add's collision to the constructor method.

--- include/Tree.h
+++ include/Tree.h
@@ -1,40 +1,23 @@
 #include <Shader.h>
 #include <Model.h>
 #include <CollidableBody.h>
-//#include <vector>
 
 class Tree {
 
 public:
 	
 	Tree();
-	
-	//glm::vec3 getTreePos();
-
-	//void setTreePos(glm::vec3& newPosition);
-
-	//void transformTree(Shader* shader, int x, int z);
-
-	//void positionate(Shader& shader);
-
-	//void storeTrees(Shader& shader);
-
 	void drawTrees(Shader& shader);
 
 private:
-	void generateTreesTransforms();
 
-private:
-	bool draw = false;
-	int x, z;
-	int cont = 1;
 	int forestSize = 50;
 	int treeSpacing = 10;
 	std::unique_ptr<class Model> treeModel;
 	CollidableBody treeBody;
-	//std::vector<glm::vec3> treeP;
-
 	std::vector<glm::mat4> treeTransform;
+
+	void generateTreesTransforms();
 		
 };
 
--- src/Tree.cpp
+++ src/Tree.cpp
@@ -3,36 +3,9 @@
 Tree::Tree() : treeBody(BoundingBox(glm::vec3(0), glm::vec3(0)), false)
 {
 	treeModel = std::make_unique<Model>("../images/Tree2/Tree.obj");
-
-	//treeP[cont] = glm::vec3(0);
-	
-    
-    //treeBody.setPosition(treeP[cont]);
-
+	//treeBody.setPosition(treeP[cont]);
     generateTreesTransforms();
 }
-
-//glm::vec3 Tree::getTreePos() {
-//	return treeP[cont];
-//}
-//
-//void Tree::setTreePos(glm::vec3& newPosition) {
-//	treeP[cont] = newPosition;
-//}
-
-// Sets the tree's position, scale, and rotation(to make some diversity)
-// x and z set the position
-//void Tree::transformTree(Shader* shader, int x, int z) {
-//    glm::mat4 model = glm::mat4(1.0f);
-//    float treeRotateOffset = rand() % 360;
-//    float treeScaleOffset = rand() % 3 + 2;
-//
-//    model = translate(model, glm::vec3(x, 0.0f, z));
-//    model = scale(model, glm::vec3(treeScaleOffset));
-//    model = rotate(model, glm::radians(treeScaleOffset), glm::vec3(0.0f, 1.0f, 0.0f));
-//
-//    shader->setMat4("model", model);
-//}
 
 void Tree::generateTreesTransforms() {
     srand(0);
@@ -66,8 +39,6 @@ void Tree::drawTrees(Shader& shader) {
     }
 
 }
-
-
 
 // Gerar uma posição randomica
 // Armazenar essa posição
